ae84a9c864795e52fe1eb0dc8ec7fe9e2f21a03d
Axel Elgenstierna <axelelgenstierna@Axels-MacBook-Pro.local>
2021-02-17 21:59:17 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT: number = 4200;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+  console.log(`app is up and listening to at localhost:${PORT}`)
+}
+bootstrap();
---
+++ src/modules/user/service/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from '../dto/user.dto';
+
+@Injectable()
+export class UserService {
+  public users: UserDto[] = [];
+
+  findAll(): UserDto[] {
+    return this.users;
+  }
+
+  create(user: UserDto): UserDto {
+    this.users.push(user);
+    return user;
+  } 
+
+  update(updatedUser: UserDto): UserDto {
+    this.users.splice(
+      this.users.indexOf(
+        this.users.find((user: UserDto): Boolean => user.id === updatedUser.id)
+      ), 1, updatedUser);
+    return updatedUser;
+  }
+
+  deleteUser(id: number): UserDto[] {
+    this.users.splice(
+      this.users.indexOf(
+        this.users.find((user: UserDto): Boolean => user.id === id)
+      ), 1);
+    return this.users;
+  }
+}
