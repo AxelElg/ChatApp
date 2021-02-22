42a07a5c033e4b56e5d43e60cd7ccf12f4091678
Axel Elgenstierna <axelelgenstierna@Axels-MacBook-Pro.local>
2021-02-22 23:38:03 +0100

    fixed the issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 // import { UserModule } from './modules/user/user.module';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UserModule } from './user/user.module';
 
 @Module({
   // imports: [UserModule],
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
-    })
+    }),
+    UserModule
   ],
   controllers: [],
   providers: [],
