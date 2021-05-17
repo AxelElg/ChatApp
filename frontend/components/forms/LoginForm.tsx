bae994469d0a7f7f0e21240f46fb911ca9bd2726
Axel Elgenstierna <axelelgenstierna@Axels-MacBook-Pro.local>
2021-05-17 22:14:14 +0200

    todays progress 17/5

--- frontend/components/forms/LoginForm.tsx
+++ frontend/components/forms/LoginForm.tsx
@@ -1,13 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function LoginForm() {
+
+  const [errorInput, setErrorInput] = useState(false)
+  
+
+  const tryLogin : any  = async event => {
+    event.preventDefault()
+
+    const userName: string = event.target.username.value;
+    const password: string = event.target.username.value;
+    
+    if (userName.length && password.length > 0) {
+      const res = await fetch('/api/login', {
+        body: JSON.stringify({
+          name: event.target.username.value,
+          pass: event.target.password.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }).then(res => res).then(result => {
+        console.log(result);  
+      }).catch(
+        setErrorInput(true)
+      )  
+    }
+  }
+
   return (
-    <form action="">
+    <form onSubmit={tryLogin} className={errorInput ? 'error' : undefined}>
       <h2>log in</h2>
       <label htmlFor="#username">user name</label>
       <input id="username" type="text" placeholder="user name" />
       <label htmlFor="#password">password</label>
       <input id="password" type="text" placeholder="password" />
+      <input type="submit" value="Submit"/>
+      {errorInput && (<div>hello</div>)}
     </form>
   )
 }
+
+
