2acad118bc916591fb43ad616045da2dc9e06571
Axel Elgenstierna <axelelgenstierna@Axels-MacBook-Pro.local>
2021-05-24 22:19:34 +0200

    24/4-21

--- frontend/components/PublicLayout/TopBar.tsx
+++ frontend/components/PublicLayout/TopBar.tsx
@@ -1,12 +1,15 @@
+import { useRouter } from 'next/router'
 
 export default function TopBar() {
+  const router = useRouter();
+
   return (
     <div className="top-bar">
       <div className="container">
           <img src="" alt="error" />
           <div className="menu">
-            <div className="menu-item">home</div>
-            <div className="menu-item">sign up</div>
+            <div className="menu-item" onClick={() => router.push('/')}>home</div>
+            <div className="menu-item" onClick={() => router.push('/login')}>sign up</div>
             <button className="btn">log in</button>
           </div>
       </div>
---
+++ frontend/pages/signup.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PublicLayout from '../components/PublicLayout';
+import SignupForm from "../components/forms/SignupForm";
+
+export default function signup() {
+  return (
+    <PublicLayout>
+      <SignupForm />
+    </PublicLayout>
+  )
+}
